7b4943e8eff00c90f6acedcc8cb6a5a3a4e31da6
Enrico <60024711+ewidodo@users.noreply.github.com>
2023-01-25 03:37:28 -0800

    Switched bullet back to hitscan

--- Assets/Resources/SunflowerGun/Scripts/SunflowerProjectile.cs
+++ Assets/Resources/SunflowerGun/Scripts/SunflowerProjectile.cs
@@ -2,7 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SunflowerProjectile : BaseBullet
+public class SunflowerProjectile : BaseHitscan
 {
-    
+    public Rigidbody trailRigidbody;
+
+
+    public override void Start()
+    {
+        Destroy(gameObject, lifespan);
+
+        ApplyRandomSpread();
+
+        if (Physics.Raycast(Camera.main.transform.position, transform.forward, out hitInfo, 200.0f, raycastCheckLayers))
+        {
+            targetPosition = hitInfo.point;
+            hasRaycastHit = true;
+        }
+        else
+        {
+            SetTargetPosition(new Ray(Camera.main.transform.position, transform.forward).GetPoint(200));
+        }
+
+        
+        if(!hasRaycastHit)
+        {
+            return;
+        }
+
+        ApplyTrailRendererForce();
+
+        ProcessDamage();
+    }
+
+
+    public void ApplyTrailRendererForce()
+    {
+        Vector3 impulseDirection = transform.forward;
+        trailRigidbody.AddForce(impulseDirection * 500f, ForceMode.VelocityChange);
+    }
 }
